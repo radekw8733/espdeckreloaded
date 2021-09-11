f2e9901234f5d3da00cfa6b98e4f96a785243a1c
Radosław Wolański <radekw8733@gmail.com>
2021-09-11 12:25:14 +0200

    Settings feature added

--- espdeckreloaded.ino
+++ espdeckreloaded.ino
@@ -5,12 +5,20 @@
 #include <Preferences.h>
 Preferences preferences;
 MD_Parola lcd = MD_Parola(MD_MAX72XX::FC16_HW,5,4);
+#pragma region various declarations
+struct t_settings {
+    int parola_speed = 30;
+    
+};
+#pragma endregion
+t_settings globalSettings;
 //MD_Parola lcd = MD_Parola(MD_MAX72XX::FC16_HW,23,18,5,4);
 void setup()
 {
     preferences.begin("espdeckreloaded");
+    loadSettings();
     lcd.begin();
-    lcd.displayText("Initializing",PA_LEFT,30,2000,PA_SCROLL_LEFT,PA_SCROLL_DOWN);
+    lcd.displayText("Initializing",PA_LEFT,globalSettings.parola_speed,2000,PA_SCROLL_LEFT,PA_SCROLL_DOWN);
     //lcd.print("Initializing");
     if (!preferences.getBool("isInitialized")) {
         WiFi.mode(WIFI_AP);
@@ -21,4 +29,8 @@ void setup()
 void loop()
 {
     lcd.displayAnimate();
+}
+
+void loadSettings() {
+    globalSettings.parola_speed = preferences.getInt("parola_speed", 30);
 }
