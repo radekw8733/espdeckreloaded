8818990c2f49d735d1407d4964aeead18de58f69
Radosław Wolański <radekw8733@gmail.com>
2021-09-14 07:16:35 +0200

    Added authentication setip

--- espdeckreloaded.ino
+++ espdeckreloaded.ino
@@ -2,14 +2,16 @@
 #include <MD_Parola.h>
 #include <SPI.h>
 #include <WiFi.h>
+#include <WebServer.h>
 #include <Preferences.h>
 Preferences preferences;
 MD_Parola lcd = MD_Parola(MD_MAX72XX::FC16_HW,5,4);
-#pragma region various declarations
+WebServer server(80);
+#pragma region VARIOUS DECLARATIONS
 struct t_settings {
     int parola_speed = 30;
-    
 };
+String accessToken;
 #pragma endregion
 t_settings globalSettings;
 //MD_Parola lcd = MD_Parola(MD_MAX72XX::FC16_HW,23,18,5,4);
@@ -24,6 +26,14 @@ void setup()
         WiFi.mode(WIFI_AP);
         WiFi.softAP("Espdeck Reloaded");
     }
+    server.on("/setup", []() {
+        if (server.arg("accessToken") != "") {
+            preferences.putString("accessToken",server.arg("accessToken"));
+            accessToken = server.arg("accessToken");
+            server.send(200,"text/plain","OK Access Token set");
+        }
+    });
+    server.begin();
 }
 
 void loop()
