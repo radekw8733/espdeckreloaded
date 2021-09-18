da4662d87a4965dafd6715b3b1414cd9ef178726
Radosław Wolański <radekw8733@gmail.com>
2021-09-18 22:52:59 +0200

    Finalized setup

--- espdeckreloaded.ino
+++ espdeckreloaded.ino
@@ -12,10 +12,11 @@ Preferences prefs;
 MD_Parola matrix = MD_Parola(MD_MAX72XX::FC16_HW,5,4);
 // MD_Parola matrix = MD_Parola(MD_MAX72XX::FC16_HW, DATA_PIN, CLK_PIN, CS_PIN);  // use software SPI on matrix
 WebServer server(80);
+WebServer apiServer(80);
 
 #pragma region VARIOUS DECLARATIONS
 String accessToken;
-bool wasResetHoldBefore;
+bool wasResetHoldBefore = false;
 long resetButtonTimeCounter;
 #include "artwork.h"
 #ifdef ADD_NEOPIXEL
@@ -51,18 +52,89 @@ void setup()
     ledStrip.setBrightness(50);
     #endif
     if (!prefs.getBool("isInitialized")) {
+        Serial.println("--- EspDeck not initialized ---");
+        Serial.println("Setting up access point...");
         WiFi.mode(WIFI_AP);
         WiFi.softAP("Espdeck Reloaded");
         server.on("/setup", []() {
-            if (server.arg("accessToken") != "") {
+            if (server.arg("accessToken") != "" && server.arg("wifiSSID") != "" && server.arg("wifiPASS") != "") {
                 prefs.putString("accessToken",server.arg("accessToken"));
+                prefs.putString("wifi_SSID",server.arg("wifiSSID"));
+                prefs.putString("wifi_PASS",server.arg("wifiPASS"));
+                if (server.arg("staticIP") == "true") {
+                    prefs.putUInt("wifi_staticIp",server.arg("wifi_staticIp").toInt());
+                    prefs.putUInt("wifi_gatewayIp",server.arg("wifi_gatewayIp").toInt());
+                    prefs.putUInt("wifi_mask",server.arg("wifi_mask").toInt());
+                }
                 accessToken = server.arg("accessToken");
                 server.send(200,"text/plain","OK Access Token set");
                 server.close();
                 prefs.putBool("isInitialized",true);
+                ESP.restart();
+            }
+            else {
+                server.send(400,"text/plain","No Access Token provided");
             }
         });
         server.begin();
+        Serial.println("Waiting for connections");
+        for (;;) {
+            if (matrix.displayAnimate()) {
+                matrix.displayClear();
+                matrix.displayText("Waiting for setup. Go to app and configure your clock",PA_CENTER,50,1000,PA_SCROLL_LEFT);
+            }
+            server.handleClient();
+        }
+    }
+    else {
+        Serial.println("Connecting to wifi " + prefs.getString("wifi_SSID","") + " ...");
+        WiFi.mode(WIFI_STA);
+        if (prefs.getBool("wifi_isStatipIpSet")) {
+            WiFi.config(
+                IPAddress(prefs.getUInt("wifi_staticIp")),
+                IPAddress(prefs.getUInt("wifi_gatewayIp")),
+                IPAddress(prefs.getUInt("wifi_mask"))
+            );
+        }
+        char wifiSSID[48];
+        char wifiPASS[48];
+        prefs.getString("wifi_SSID","").toCharArray(wifiSSID,sizeof(wifiSSID));
+        prefs.getString("wifi_PASS","").toCharArray(wifiPASS,sizeof(wifiPASS));
+        WiFi.begin(wifiSSID,wifiPASS);
+        int retryCounter = 0;
+        while (WiFi.status() != WL_CONNECTED) {
+            retryCounter += 1;
+            Serial.println(".");
+            if (retryCounter > 20) {
+                if (matrix.displayAnimate()) {
+                    matrix.displayClear();
+                    matrix.displayText("Still connecting... reset if you entered wrong wifi info",PA_CENTER,50,1000,PA_SCROLL_LEFT);
+                }
+                checkResetButton();
+                delay(50);
+            }
+            else {
+                delay(500);
+            }
+        }
+        Serial.println("CONNECTED");
+        Serial.println("ip: " + WiFi.localIP());
+        apiServer.on("/toggleModule", [] {
+            if (apiServer.arg("moduleID") != "") {
+                for (int i = 0; i < modules.size(); i++) {
+                    if (modules[i]->id == apiServer.arg("moduleID")) {
+                        modules[i]->isEnabled = !modules[i]->isEnabled;
+                        server.send(200,"text/plain","Module " + modules[i]->moduleName + " is now " + modules[i]->isEnabled ? "enabled" : "disabled");
+                        return;
+                    }
+                }
+                server.send(404,"text/plain","No module found");
+            }
+            else {
+                server.send(400,"text/plain","No module id provided");
+            }
+        });
+        apiServer.begin();
     }
     Serial.println("Initializing modules...");
     Serial.println("Modules in array: " + String(modules.size()));
@@ -87,6 +159,7 @@ void loop()
             while (millisStart + (modules[i]->matrixRefreshRate - (millisEnd - millisStart)) > millis()) {
                 checkResetButton();
                 matrix.displayAnimate();
+                apiServer.handleClient();
             }
         }
     }
@@ -95,15 +168,14 @@ void loop()
 void checkResetButton() {
     if (digitalRead(14) == LOW) {
         if (wasResetHoldBefore == true) {
-            if (millis() > resetButtonTimeCounter + 3000) {
+            if (millis() > (resetButtonTimeCounter + 3000)) {
                 prefs.clear();
                 ESP.restart();
             }
-            else {
-                resetButtonTimeCounter = millis();
-            }
         }
         else {
+            Serial.println("Button state change detected!");
+            resetButtonTimeCounter = millis();
             wasResetHoldBefore = true;
         }
     }
